eb3cec520718f22d81016792df714ecdf5065efd
Natalino Gomes <intgomes657@gmail.com>
2025-09-28 00:52:43 +0100

    update product model

--- models/productModel.js
+++ models/productModel.js
@@ -1,31 +1,83 @@
 import mongoose from "mongoose";
 
+
+const reviewsSchema = new mongoose.schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+
+    name: { 
+        type: String, 
+        required: true 
+    },
+
+    rating: { 
+        type: Number, 
+        required: true 
+    },
+
+    comment: { 
+        type: String, 
+        required: true 
+    },
+    
+}, {
+    timestamps: true
+});
+
 const productSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    name: { type: String, required: true },
-    image: { type: String, required: true },
-    category: { type: String, required: true },
-    price: { type: String, required: true },
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+
+    name: { 
+        type: String, 
+        required: true 
+    },
+
+    image: { 
+        type: String, 
+        required: true 
+    },
+
+    category: { 
+        type: String, 
+        required: true 
+    },
+
+    price: { 
+        type: Number, 
+        required: true 
+    },
+
     reviews: [reviewsSchema],
-    rating: { type: Number, required: true, default: 0 },
-    numReviews: { type: Number, required: true, default: 0 },
+
+    rating: { 
+        type: Number, 
+        required: true, 
+        default: 0 
+    },
+
+    numReviews: { 
+        type: Number, 
+        required: true, 
+        default: 0 
+    },
+
     countInStock: {
         type: Number,
         required: true,
         default: 0 
     },
 
-}, {
-    timestamps: true
-
-});
-
-
-const reviewsSchema = new mongoose.schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    name: { type: String, required: true },
-    rating: { type: Number, required: true },
-    comment: { type: String, required: true },
-}, {
+},{
     timestamps: true
 });
+
+
+const Product = mongoose.model('product', productSchema);
+export default Product;
