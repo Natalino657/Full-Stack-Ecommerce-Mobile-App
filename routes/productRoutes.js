b3b74d121a16d940fd74bb0dbb4923eb959896cd
Natalino Gomes <intgomes657@gmail.com>
2025-10-09 18:47:02 +0100

    secure routes and prep backend for diferents data format recive

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,15 +7,16 @@ import {
   deleteProduct,
   createProductReview,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getProducts).post(createProduct);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .put(updateProduct)
-  .delete(deleteProduct);
-router.route("/:id/reviews").post(createProductReview);
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
--- server.js
+++ server.js
@@ -4,12 +4,20 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+import cookieParser from "cookie-parser";
+import cors from "cookie-parser";
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
 const port = 5300;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
